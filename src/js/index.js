c8146faa0ab06103193d276a4dd12a788ffbc196
gao <1210143878@qq.com>
2018-05-22 21:02:36 +0800

    nhh

--- src/js/index.js
+++ src/js/index.js
@@ -34,19 +34,93 @@ require(["config"], function(){
 				$(".myOrder").css({"display":"none"});
 			});
 			
-			var num=$(".ii");
-			var len=num.length;
-			var circles="";
-			var=lis="";
-			for(var i=0;i<len;i++){
-				lis+=`<li ${i==0?'style="display:block;"':''}>
-						<a href="${im}">
-						</a>
-					</li>`;
-			}
+			var lis=$(".ii");
+			var len=lis.length;
+			var cricles=$("#pointd").children();
+			var currentIndex=0;
+			var nextIndex=1;
 			
-			$("#lunbotu").innerHTML=circles;
+			var timer=setInterval(move,2000);
+				
+			
+			
+			function move(){
+				lis.eq(currentIndex).stop().fadeOut();					
+				lis.eq(nextIndex).stop().fadeIn();
+//				lis.eq(nextIndex).css({"transform":"scale(0.9,0.9)","transition":"all 2s"});
+//				lis.eq(currentIndex).css("");
+				cricles.eq(currentIndex).removeClass("pp");
+				cricles.eq(nextIndex).addClass("pp");
+				
+				currentIndex=nextIndex;
+				nextIndex++;
+				if(nextIndex>len-1){
+					nextIndex=0;
+			}
+			};
+			
+			
+			$(".lunbo").hover(function(){
+				clearInterval(timer);
+				$(".prev").css({"display":"block"});
+				$(".next").css({"display":"block"});
+//				console.log("456");
+			},function(){
+				setInterval(move,2000);
+//				console.log("123");
+			});
+			$(".prev").on("click",function(){
+				nextIndex=currentIndex-1;
+				if(nextIndex<0){
+					nextIndex=len-1;
+				}
+				move();
+			});
+			$(".next").on("click",function(){
+				move();
+			});
+			cricles.on("mouseenter",function(e){
+				var _index=$(e.target).index();
+				if(currentIndex==_index){
+					return;
+				}
+				nextIndex=_index;
+				move();
+			});
+			
+			$(".allgoods").on("mouseenter",function(){
+				$("#goodslist").css({"display":"block"});
+			});
+			$(".allgoods").on("mouseleave",function(){
+				$("#goodslist").css({"display":"none"});
+			});
+			
+			$("#goodslist").on("mouseenter",function(){
+				$("#goodslist").css({"display":"block"});
+			});
+			$("#goodslist").on("mouseleave",function(){
+				$("#goodslist").css({"display":"none"});
+			});
+			
+			var goodsnum=$("#goodslist").children();
+			goodsnum.eq(0).on("mouseenter",function(){
+				$(".box1").css({"display":"block"});
+				
+			});
+			goodsnum.eq(0).on("mouseleave",function(){
+				$(".box1").css({"display":"none"});
+			});
+			$(".box1").on("mouseenter",function(){
+				$(".box1").css({"display":"block"});
+				$("#goodslist").css({"display":"block"});
+			});
+			$(".box1").on("mouseleave",function(){
+				$(".box1").css({"display":"none"});
+				$("#goodslist").css({"display":"none"});
+			});
+			
+			
+//		width:1193
 		});
-
 	});
 });
